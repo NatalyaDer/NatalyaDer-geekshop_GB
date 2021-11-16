dbf220019bae51d9e850cc1eaaf66d3967fcc071
NatalyaDer <68365980+NatalyaDer@users.noreply.github.com>
2021-11-16 21:52:46 +0700

    Lesson 7 (#8)
    * lesson_7. Adminapp.
    
    * lesson_7. Admin link and firewall.
    
    * lesson_7. CRUD users.
    
    * lesson_7. CRUD categories.
    
    * Lesson 7. Code style fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     "mainapp",
     "authnapp",
     "basketapp",
+    "adminapp",
 ]
 
 # Auth model
